3ae20f37d1a751de09802e73b3258d7d206e9217
jswan <jswannieman@gmail.com>
2023-07-21 13:22:47 +1200

    Commit App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 
-const PropChangeWatch = ({ a, b }) => {
+const PropChangeWatch = ({ x, y, z }) => {
   useEffect(() => {
-    console.log("Prop a has changed and was detected in the useEffect Hook");
-  }, [a]);
+    alert(
+      `Prop 'x' has changed and was detected in the useEffect Hook - The value is now ${x}`
+    );
+  }, [x]);
 
   return (
     <div>
       <h4>
-        2 props on display: a={a} and b={b}
+        3 props on display: x = {x} and y = {y} and z = {z}
       </h4>
-      <h4>The useEffect Hook has detected that Prop "a" has changed to {a}</h4>
+      <h4>The useEffect Hook has detected that Prop "x" has changed to {x}</h4>
     </div>
   );
 };
@@ -18,12 +20,14 @@ const PropChangeWatch = ({ a, b }) => {
 const App = () => {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
 
   return (
     <div>
-      <PropChangeWatch a={count1} b={count2} />
+      <PropChangeWatch x={count1} y={count2} z={count3} />
       <button onClick={() => setCount1(count1 + 1)}>Increment count1</button>
       <button onClick={() => setCount2(count2 + 1)}>Increment count2</button>
+      <button onClick={() => setCount3(count3 + 1)}>Increment count3</button>
     </div>
   );
 };
